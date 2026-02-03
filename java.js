2f5f34fefecc00deaeae8021329b7d32f152bae4
24tupM3-4 <pimnattaphondeeprasertkul@gmail.com>
2026-02-03 13:12:02 +0700

    Refactor buddy animation and speech functionality

--- java.js
+++ java.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   /* main settings */
   const SPRITE = 500;
-  const SCALE = 400 / SPRITE;
+  const SCALE = 500 / SPRITE;
   const FRAME = SPRITE;
   const DIAG = 0.7071;
 
@@ -373,6 +373,7 @@ page = "/" + page.split("/").pop();
   }
   follow();
 });
+
 
 
 

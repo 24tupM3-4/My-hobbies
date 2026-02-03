cb840d1a844c0fe3abc0649de468c3a99b3e5c2b
24tupM3-4 <pimnattaphondeeprasertkul@gmail.com>
2026-02-03 13:16:38 +0700

    Refactor buddy animation settings and dialogue

--- java.js
+++ java.js
@@ -4,8 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
   if (!buddy) return;
 
   /* main settings */
-  const SPRITE = 700;
-  const SCALE = 400 / SPRITE;
+  const SPRITE = 350;
+  const SCALE = 200 / SPRITE;
   const FRAME = SPRITE;
   const DIAG = 0.7071;
 
@@ -373,6 +373,7 @@ page = "/" + page.split("/").pop();
   }
   follow();
 });
+
 
 
 
